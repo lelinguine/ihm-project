9797462fd8aadc8c6ffc51a3b04e67de74d8bae4
Valentin <luginbuhlvalentin@gmail.com>
2025-09-23 11:27:30 +0200

    Init
    - Initialisation de Svelte + Vite
    - Ajout de tailwind
    - Ajout de la bibliothèque de composants shadcn
    - Ajout du système de routing avec svelte-spa-router

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Router, { link } from 'svelte-spa-router';
+
+  import Home from './views/Home.svelte';
+  import About from './views/About.svelte';
+
+  const routes = {
+    '/': Home,
+    '/about': About,
+  };
+</script>
+
+<main>
+  <!-- Navigation -->
+  <nav class="p-4 bg-gray-100 border-b">
+    <ul class="flex space-x-4">
+      <li>
+        <a href="/" use:link class="text-blue-600 hover:text-blue-800">Accueil</a>
+      </li>
+      <li>
+        <a href="/about" use:link class="text-blue-600 hover:text-blue-800">À propos</a>
+      </li>
+    </ul>
+  </nav>
+
+  <!-- Contenu des pages -->
+  <div class="p-4">
+    <Router {routes}/>
+  </div>
+</main>
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+// https://vite.dev/config/
+export default defineConfig({ 
+    plugins: [tailwindcss(), svelte()],
+    resolve: {
+    alias: {
+      $lib: new URL("./src/lib", import.meta.url).pathname,
+    },
+  },
+ });
