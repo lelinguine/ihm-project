2b7b63e9d2e935aaf6debbd2ec99cb64546abde3
ayla <158297112+ayladne@users.noreply.github.com>
2025-11-12 16:53:34 +0100

    Add Feed component to display tweets

---
+++ src/lib/components/Feed.svelte
@@ -0,0 +1,26 @@
+<script>
+  import TweetCard from './TweetCard.svelte';
+  import { tweets } from '$lib/stores/tweets';
+
+  let selectedTweet = null;
+
+  function handleReply(tweet) {
+    selectedTweet = tweet;
+    // Ouvrir le composeur de réponse
+  }
+</script>
+
+<div class="feed">
+  {#each $tweets as tweet (tweet.id)}
+    <TweetCard {tweet} onReply={handleReply} />
+  {/each}
+</div>
+
+<style>
+  .feed {
+    max-width: 600px;
+    margin: 0 auto;
+    border-left: 1px solid #e1e8ed;
+    border-right: 1px solid #e1e8ed;
+  }
+</style>
